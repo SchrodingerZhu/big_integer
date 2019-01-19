13f5a51207b14ce79e29e218b39e52211043f6bd
SchrodingerZhu <jimmyzhu1999@protonmail.com>
2019-01-19 09:42:23 +0800

    add basic structures.

--- big_integer.cpp
+++ big_integer.cpp
@@ -2,6 +2,40 @@
 
 #include <iostream>
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+template<typename Transformer, std::size_t radix,
+        template<typename _Tp, typename _Alloc = std::allocator<_Tp>> typename Container, typename BaseType>
+struct BigInteger<Transformer, radix, Container, BaseType>::Impl {
+    bool sign{true};
+    Container<BaseType> digits{};
+    static Transformer transformer{};
+};
+
+template<typename Transformer, std::size_t radix,
+        template<typename _Tp, typename _Alloc = std::allocator<_Tp>> typename Container, typename BaseType>
+BigInteger<Transformer, radix, Container, BaseType>::BigInteger() : pImpl(std::make_unique<Impl>()) {}
+
+template<typename Transformer, std::size_t radix,
+        template<typename _Tp, typename _Alloc = std::allocator<_Tp>> typename Container, typename BaseType>
+BigInteger<Transformer, radix, Container, BaseType>::~BigInteger() = default;
+
+template<typename Transformer, std::size_t radix,
+        template<typename _Tp, typename _Alloc = std::allocator<_Tp>> typename Container, typename BaseType>
+BigInteger<Transformer, radix, Container, BaseType>::BigInteger(BigInteger &&rhs) noexcept = default;
+
+template<typename Transformer, std::size_t radix,
+        template<typename _Tp, typename _Alloc = std::allocator<_Tp>> typename Container, typename BaseType>
+BigInteger<Transformer, radix, Container, BaseType> &
+BigInteger<Transformer, radix, Container, BaseType>::operator=(BigInteger &&rhs) noexcept = default;
+
+template<typename Transformer, std::size_t radix,
+        template<typename _Tp, typename _Alloc = std::allocator<_Tp>> typename Container, typename BaseType>
+BigInteger<Transformer, radix, Container, BaseType>::BigInteger(const BigInteger &rhs) : pImpl(
+        std::make_unique<Impl>(*rhs.pImpl)) {}
+
+template<typename Transformer, std::size_t radix,
+        template<typename _Tp, typename _Alloc = std::allocator<_Tp>> typename Container, typename BaseType>
+BigInteger<Transformer, radix, Container, BaseType> &
+BigInteger<Transformer, radix, Container, BaseType>::operator=(const BigInteger &rhs) {
+    *pImpl = *rhs.pImpl;
+    return *this;
 }
--- big_integer.h
+++ big_integer.h
@@ -1,4 +1,92 @@
 #ifndef BIG_INTEGER_LIBRARY_H
 #define BIG_INTEGER_LIBRARY_H
 
+#include <cstddef>
+#include <memory>
+#include <vector>
+#include "transformer.h"
+
+template<typename Transformer = NTTTransformer<>,
+        std::size_t radix = 4,
+        template<typename _Tp, typename _Alloc = std::allocator<_Tp>> typename Container = std::vector,
+        typename BaseType = int>
+class BigInteger {
+    struct Impl;
+    std::unique_ptr<struct Impl> pImpl;
+public:
+    BigInteger();
+
+    BigInteger(const long long &number);
+
+    BigInteger(const int &number);
+
+    BigInteger(const short &number);
+
+    BigInteger(const char &number);
+
+    BigInteger(const unsigned long long &number);
+
+    BigInteger(const unsigned int &number);
+
+    BigInteger(const unsigned short &number);
+
+    BigInteger(const unsigned char &number);
+
+    BigInteger(const bool &number);
+
+    explicit BigInteger(const std::string &str);
+
+    BigInteger(const BigInteger &rhs);
+
+    BigInteger(BigInteger &&rhs) noexcept;
+
+    ~BigInteger();
+
+    BigInteger &operator=(const BigInteger &rhs);
+
+    BigInteger &operator=(BigInteger &&rhs) noexcept;
+
+    BigInteger operator+(const BigInteger &rhs) const;
+
+    BigInteger operator-(const BigInteger &rhs) const;
+
+    BigInteger operator*(const BigInteger &rhs) const;
+
+    BigInteger operator/(const BigInteger &rhs) const;
+
+    BigInteger operator^(const BigInteger &rhs) const;
+
+    BigInteger operator&(const BigInteger &rhs) const;
+
+    BigInteger &operator+=(const BigInteger &rhs);
+
+    BigInteger &operator-=(const BigInteger &rhs);
+
+    BigInteger &operator*=(const BigInteger &rhs);
+
+    BigInteger &operator/=(const BigInteger &rhs);
+
+    BigInteger &operator^=(const BigInteger &rhs);
+
+    BigInteger &operator&=(const BigInteger &rhs);
+
+    BigInteger operator!() const;
+
+    BigInteger &operator++();
+
+    const BigInteger operator++(int);
+
+    BigInteger &operator--();
+
+    const BigInteger operator--(int);
+
+    BigInteger operator~() const;
+
+    BigInteger slow_multiply(const BigInteger &rhs) const;
+
+    bool is_negative() const noexcept;
+    // BigInteger approx_sqrt() const noexcept;
+
+};
+
 #endif
